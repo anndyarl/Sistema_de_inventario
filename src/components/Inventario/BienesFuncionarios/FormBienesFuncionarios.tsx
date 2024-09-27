9cfa12dd2345e7e2ffaa9aac59a2502df08ddf3e
Andy Ariel Riquelme Larenas <andy.riquelme@ssmso.cl>
2024-09-27 09:17:59 -0300

    se agrega fileupFileDropzone

--- src/components/Inventario/BienesFuncionarios/FormBienesFuncionarios.tsx
+++ src/components/Inventario/BienesFuncionarios/FormBienesFuncionarios.tsx
@@ -33,21 +33,66 @@ const FileDropzone = () => {
   };
 
   return (
-    <div
-      className={`dropzone ${isDragging ? 'dragging' : ''}`}
-      onDrop={handleDrop}
-      onDragOver={handleDragOver}
-      onDragLeave={handleDragLeave}
-      onClick={() => document.getElementById('fileInput')?.click()}
-    >
-      {selectedFile1 && selectedFile2 ? (
-        <p>
-          Archivos seleccionados: {selectedFile1.name}, {selectedFile2.name}
-        </p>
-      ) : (
-        <p>Arrastra y suelta los archivos aquí, o haz clic para seleccionar dos archivos</p>
-      )}
-    </div>
+    <Layout>
+      <strong>Ingeso de Bienes del Funcionario</strong>
+      {/* <div
+        className="file-upload"
+        style={{ border: '1px solid #ccc', padding: '20px', borderRadius: '5px' }}
+      > */}
+      <form /* onSubmit={handleFormSubmit}*/>
+        {/* Inputs agregados */}
+        <div className="input-container">
+          <label className="form-label">Rut Funcionario</label>
+          <input
+            type="text"
+            // value={inputValue1}
+            // onChange={(e) => setInputValue1(e.target.value)}
+            className="form-control"
+            placeholder="Ingrese rut del funcionario"
+          />
+          <label className="form-label">Destino</label>
+          <input
+            type="text"
+            // value={inputValue2}
+            // onChange={(e) => setInputValue2(e.target.value)}
+            className="form-control"
+            placeholder="Ingrese el destino del bien"
+          />
+        </div>
+        {/* Funcionalidad para subir archivos */}
+        <label className="form-label">Subir archivos</label>
+        <div
+          className={`dropzone ${isDragging ? 'dragging' : ''}`}
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+          onDragLeave={handleDragLeave}
+          onClick={() => document.getElementById('fileInput')?.click()}
+        >
+          {selectedFile1 && selectedFile2 ? (
+            <p>
+              Archivos seleccionados: {selectedFile1.name}, {selectedFile2.name}
+            </p>
+          ) : (
+            <p>Arrastra y suelta los archivos aquí, o haz clic para seleccionar dos archivos</p>
+          )}
+        </div>
+        <input
+          type="file"
+          // onChange={handleFileChange}
+          id="fileInput"
+          className="file-input"
+          multiple
+        />
+        <button type="button" /*onClick={handleFileSubmit} */ className="submit-button">
+          Enviar archivos
+        </button>
+        {/* <button type="submit" className="submit-button">
+            Enviar formulario completo
+          </button> */}
+      </form>
+      {/* </div> */}
+    </Layout>
+
   );
 };
 
